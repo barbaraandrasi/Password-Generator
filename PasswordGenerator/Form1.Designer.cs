276df419b0361c3d0f4b955a6c62bd3156a96c8d
barbaraandrasi <andrasibarbara@yahoo.com>
2019-06-08 21:51:42 +0300

    first commit

---
+++ PasswordGenerator/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace PasswordGenerator
+{
+    partial class GeneratorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lengthField = new System.Windows.Forms.NumericUpDown();
+            this.groupBox = new System.Windows.Forms.GroupBox();
+            this.letterTypeButtonGroup = new System.Windows.Forms.GroupBox();
+            this.bothButton = new System.Windows.Forms.RadioButton();
+            this.lowerCaseButton = new System.Windows.Forms.RadioButton();
+            this.upperCaseButton = new System.Windows.Forms.RadioButton();
+            this.contentRadioButtonGroup = new System.Windows.Forms.GroupBox();
+            this.specialCharButton = new System.Windows.Forms.RadioButton();
+            this.lettersButton = new System.Windows.Forms.RadioButton();
+            this.alphanumericButton = new System.Windows.Forms.RadioButton();
+            this.lengthLabel = new System.Windows.Forms.Label();
+            this.generateButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.lengthField)).BeginInit();
+            this.groupBox.SuspendLayout();
+            this.letterTypeButtonGroup.SuspendLayout();
+            this.contentRadioButtonGroup.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lengthField
+            // 
+            this.lengthField.AccessibleDescription = "Length";
+            this.lengthField.AccessibleName = "Length";
+            this.lengthField.Location = new System.Drawing.Point(86, 26);
+            this.lengthField.Name = "lengthField";
+            this.lengthField.Size = new System.Drawing.Size(159, 20);
+            this.lengthField.TabIndex = 0;
+            // 
+            // groupBox
+            // 
+            this.groupBox.Controls.Add(this.letterTypeButtonGroup);
+            this.groupBox.Controls.Add(this.contentRadioButtonGroup);
+            this.groupBox.Controls.Add(this.lengthLabel);
+            this.groupBox.Controls.Add(this.lengthField);
+            this.groupBox.Location = new System.Drawing.Point(9, 9);
+            this.groupBox.Margin = new System.Windows.Forms.Padding(0);
+            this.groupBox.Name = "groupBox";
+            this.groupBox.Padding = new System.Windows.Forms.Padding(10);
+            this.groupBox.Size = new System.Drawing.Size(258, 243);
+            this.groupBox.TabIndex = 1;
+            this.groupBox.TabStop = false;
+            this.groupBox.Text = "Settings";
+            // 
+            // letterTypeButtonGroup
+            // 
+            this.letterTypeButtonGroup.Controls.Add(this.bothButton);
+            this.letterTypeButtonGroup.Controls.Add(this.lowerCaseButton);
+            this.letterTypeButtonGroup.Controls.Add(this.upperCaseButton);
+            this.letterTypeButtonGroup.Location = new System.Drawing.Point(16, 147);
+            this.letterTypeButtonGroup.Name = "letterTypeButtonGroup";
+            this.letterTypeButtonGroup.Size = new System.Drawing.Size(229, 85);
+            this.letterTypeButtonGroup.TabIndex = 3;
+            this.letterTypeButtonGroup.TabStop = false;
+            this.letterTypeButtonGroup.Text = "Letter type";
+            // 
+            // bothButton
+            // 
+            this.bothButton.AutoSize = true;
+            this.bothButton.Location = new System.Drawing.Point(6, 65);
+            this.bothButton.Name = "bothButton";
+            this.bothButton.Size = new System.Drawing.Size(131, 17);
+            this.bothButton.TabIndex = 4;
+            this.bothButton.TabStop = true;
+            this.bothButton.Text = "Lower and upper case";
+            this.bothButton.UseVisualStyleBackColor = true;
+            // 
+            // lowerCaseButton
+            // 
+            this.lowerCaseButton.AutoSize = true;
+            this.lowerCaseButton.Location = new System.Drawing.Point(6, 19);
+            this.lowerCaseButton.Name = "lowerCaseButton";
+            this.lowerCaseButton.Size = new System.Drawing.Size(80, 17);
+            this.lowerCaseButton.TabIndex = 2;
+            this.lowerCaseButton.TabStop = true;
+            this.lowerCaseButton.Text = "Lower case";
+            this.lowerCaseButton.UseVisualStyleBackColor = true;
+            // 
+            // upperCaseButton
+            // 
+            this.upperCaseButton.AutoSize = true;
+            this.upperCaseButton.Location = new System.Drawing.Point(6, 42);
+            this.upperCaseButton.Name = "upperCaseButton";
+            this.upperCaseButton.Size = new System.Drawing.Size(80, 17);
+            this.upperCaseButton.TabIndex = 3;
+            this.upperCaseButton.TabStop = true;
+            this.upperCaseButton.Text = "Upper case";
+            this.upperCaseButton.UseVisualStyleBackColor = true;
+            // 
+            // contentRadioButtonGroup
+            // 
+            this.contentRadioButtonGroup.Controls.Add(this.specialCharButton);
+            this.contentRadioButtonGroup.Controls.Add(this.lettersButton);
+            this.contentRadioButtonGroup.Controls.Add(this.alphanumericButton);
+            this.contentRadioButtonGroup.Location = new System.Drawing.Point(16, 56);
+            this.contentRadioButtonGroup.Name = "contentRadioButtonGroup";
+            this.contentRadioButtonGroup.Size = new System.Drawing.Size(229, 85);
+            this.contentRadioButtonGroup.TabIndex = 2;
+            this.contentRadioButtonGroup.TabStop = false;
+            this.contentRadioButtonGroup.Text = "Password contains";
+            // 
+            // specialCharButton
+            // 
+            this.specialCharButton.AutoSize = true;
+            this.specialCharButton.Location = new System.Drawing.Point(6, 65);
+            this.specialCharButton.Name = "specialCharButton";
+            this.specialCharButton.Size = new System.Drawing.Size(213, 17);
+            this.specialCharButton.TabIndex = 4;
+            this.specialCharButton.TabStop = true;
+            this.specialCharButton.Text = "Letters, numbers and special characters";
+            this.specialCharButton.UseVisualStyleBackColor = true;
+            // 
+            // lettersButton
+            // 
+            this.lettersButton.AutoSize = true;
+            this.lettersButton.Location = new System.Drawing.Point(6, 19);
+            this.lettersButton.Name = "lettersButton";
+            this.lettersButton.Size = new System.Drawing.Size(79, 17);
+            this.lettersButton.TabIndex = 2;
+            this.lettersButton.TabStop = true;
+            this.lettersButton.Text = "Letters only";
+            this.lettersButton.UseVisualStyleBackColor = true;
+            // 
+            // alphanumericButton
+            // 
+            this.alphanumericButton.AutoSize = true;
+            this.alphanumericButton.Location = new System.Drawing.Point(6, 42);
+            this.alphanumericButton.Name = "alphanumericButton";
+            this.alphanumericButton.Size = new System.Drawing.Size(121, 17);
+            this.alphanumericButton.TabIndex = 3;
+            this.alphanumericButton.TabStop = true;
+            this.alphanumericButton.Text = "Letters and numbers";
+            this.alphanumericButton.UseVisualStyleBackColor = true;
+            // 
+            // lengthLabel
+            // 
+            this.lengthLabel.AutoSize = true;
+            this.lengthLabel.Location = new System.Drawing.Point(19, 28);
+            this.lengthLabel.Name = "lengthLabel";
+            this.lengthLabel.Size = new System.Drawing.Size(40, 13);
+            this.lengthLabel.TabIndex = 1;
+            this.lengthLabel.Text = "Length";
+            // 
+            // generateButton
+            // 
+            this.generateButton.Location = new System.Drawing.Point(79, 265);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(114, 23);
+            this.generateButton.TabIndex = 2;
+            this.generateButton.Text = "Generate";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
+            // 
+            // GeneratorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(276, 300);
+            this.Controls.Add(this.generateButton);
+            this.Controls.Add(this.groupBox);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "GeneratorForm";
+            this.ShowIcon = false;
+            this.Text = "Password generator";
+            ((System.ComponentModel.ISupportInitialize)(this.lengthField)).EndInit();
+            this.groupBox.ResumeLayout(false);
+            this.groupBox.PerformLayout();
+            this.letterTypeButtonGroup.ResumeLayout(false);
+            this.letterTypeButtonGroup.PerformLayout();
+            this.contentRadioButtonGroup.ResumeLayout(false);
+            this.contentRadioButtonGroup.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown lengthField;
+        private System.Windows.Forms.GroupBox groupBox;
+        private System.Windows.Forms.Label lengthLabel;
+        private System.Windows.Forms.GroupBox contentRadioButtonGroup;
+        private System.Windows.Forms.RadioButton specialCharButton;
+        private System.Windows.Forms.RadioButton lettersButton;
+        private System.Windows.Forms.RadioButton alphanumericButton;
+        private System.Windows.Forms.GroupBox letterTypeButtonGroup;
+        private System.Windows.Forms.RadioButton bothButton;
+        private System.Windows.Forms.RadioButton lowerCaseButton;
+        private System.Windows.Forms.RadioButton upperCaseButton;
+        private System.Windows.Forms.Button generateButton;
+    }
+}
+
---
+++ PasswordGenerator/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PasswordGenerator
+{
+    public partial class GeneratorForm : Form
+    {
+        private List<char> letterList;
+        private List<char> numberList;
+        private List<char> specialList;
+
+        private Random random;
+  
+
+        public GeneratorForm()
+        {
+            InitializeComponent();
+            lettersButton.Checked = true;
+            lowerCaseButton.Checked = true;
+
+            letterList = new List<char>(new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' });
+            numberList = new List<char>(new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' });
+            specialList = new List<char>(new char[] { '.', ',', '_', '-', '?', '!', ';', ':', '$' });
+            random = new Random();
+        }
+
+
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            int length = (int) lengthField.Value;
+            RadioButton contentTypeRadioButton = contentRadioButtonGroup.Controls.OfType<RadioButton>().FirstOrDefault(r => r.Checked);
+            RadioButton letterTypeRadioButton = letterTypeButtonGroup.Controls.OfType<RadioButton>().FirstOrDefault(r => r.Checked);
+             if (isValidSettings(length, contentTypeRadioButton, letterTypeRadioButton))
+            {
+                List<char> characterList = getAppropriateCharacters(contentTypeRadioButton, letterTypeRadioButton);
+                string generatedPassword = "";
+                do
+                {
+                    generatedPassword = "";
+                    for (int i = 0; i < length; i++)
+                    {
+                        generatedPassword += characterList.ElementAt(random.Next(characterList.Count));
+                    }
+
+                } while (!isConditionsMeet(generatedPassword, contentTypeRadioButton, letterTypeRadioButton));
+
+                MessageBox.Show(generatedPassword);
+            }
+            else
+            {          
+                MessageBox.Show("Settings are not valid!");
+            }
+        }
+
+        private bool isValidSettings(int length, RadioButton contentTypeRadioButton, RadioButton letterTypeRadioButton)
+        {
+            bool numericCharacter = contentTypeRadioButton != lettersButton;
+            bool specialCharacter = contentTypeRadioButton == specialCharButton;
+
+            bool loweCase = letterTypeRadioButton != upperCaseButton;
+            bool upperCase = letterTypeRadioButton != lowerCaseButton;
+
+            if (length < 1)
+            {
+                return false;
+            }
+
+            if (length < 2 && (numericCharacter || specialCharacter || (loweCase && upperCase)))
+            {
+                return false;
+            }
+
+            if (length <3 && (specialCharacter || (loweCase && upperCase && numericCharacter)))
+            {
+                return false;
+            }
+
+            if (length < 4 && ((loweCase && upperCase && specialCharacter)))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool isConditionsMeet(string value, RadioButton contentTypeRadioButton, RadioButton letterTypeRadioButton)
+        {
+            bool numericCharacter = contentTypeRadioButton != lettersButton;
+            bool specialCharacter = contentTypeRadioButton == specialCharButton;
+
+            bool loweCase = letterTypeRadioButton != upperCaseButton;
+            bool upperCase = letterTypeRadioButton != lowerCaseButton;
+
+            bool rightSolutionNumeric = false;
+            bool rightSolutionSpecial = false;
+            bool rightSolutionLower = false;
+            bool rightSolutionUpper = false;
+
+
+            if (numericCharacter)
+            {
+                foreach (var v in value)
+                {
+                    if (numberList.Contains(v))
+                    {
+                        rightSolutionNumeric = true;
+                    }
+                }
+            }
+
+            if (specialCharacter)
+            {
+                foreach (var v in value)
+                {                
+                    if (specialList.Contains(v))
+                    {
+                        rightSolutionSpecial = true;
+                    }
+                }
+            }
+
+            if (loweCase)
+            {
+                foreach (var v in value)
+                {
+                    if (letterList.Contains(v))
+                    {
+                        rightSolutionLower = true;
+                    }
+                }
+            }
+
+            if (upperCase)
+            {
+                foreach (var v in value)
+                {
+                    if (letterList.Select(x => Char.ToUpper(x)).Contains(v))
+                    {
+                        rightSolutionUpper = true;
+                    }
+                }
+            }
+
+            return (numericCharacter ? rightSolutionNumeric :
+                true) && (specialCharacter ? rightSolutionSpecial :
+                true) && (loweCase ? rightSolutionLower :
+                true) && (upperCase ? rightSolutionUpper : true);
+        }
+
+        private List<char> getAppropriateCharacters(RadioButton contentTypeRadioButton, RadioButton letterTypeRadioButton)
+        {
+            List<char> characterList = new List<char>();
+
+            characterList.AddRange(getCharactersByLetterType(letterTypeRadioButton));
+
+            if (contentTypeRadioButton == alphanumericButton)
+            {
+                characterList.AddRange(numberList);
+            }
+            else if (contentTypeRadioButton == specialCharButton)
+            {
+                characterList.AddRange(numberList);
+                characterList.AddRange(specialList);         
+            }
+      
+            return characterList;
+        }
+
+        private List<char> getCharactersByLetterType(RadioButton letterTypeRadioButton)
+        {
+            List<char> characterList = new List<char>();
+
+            if (letterTypeRadioButton == lowerCaseButton)
+            {
+                characterList.AddRange(letterList);
+            }
+            else if (letterTypeRadioButton == upperCaseButton)
+            {
+                characterList.AddRange(letterList.Select(x => Char.ToUpper(x)));
+            }
+            else
+            {
+                characterList.AddRange(letterList);
+                characterList.AddRange(letterList.Select(x => Char.ToUpper(x)));
+            }
+            return characterList;
+        }
+    }
+}
